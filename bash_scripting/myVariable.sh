c1def0736e4d2edefc4b9cc92b663c3eebaa446b
Sahil <sahilsharma2000dtc@gmail.com>
2025-10-12 16:42:53 +0530

    mathematical expression in bash

--- bash_scripting/myVariable.sh
+++ bash_scripting/myVariable.sh
@@ -6,3 +6,14 @@ myAge="25"
 
 echo "My name is: $myName and my age is: $myAge"
 echo "I'm $myAge years old."
+
+# using  math in bash script
+myNum1=100
+myNum2=50
+
+echo "Mathematical expressions": 
+expr $myNum1 + $myNum2
+expr $myNum1 - $myNum2
+expr $myNum1 / $myNum2
+expr $myNum1 \* $myNum2 #special case here using escape literal \
+
